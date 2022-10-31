40c2184d4474054109813dd74f7ce57825764a41
korney <madkorney@gmail.com>
2022-10-31 19:30:22 +0300

    feat: implement checkbox component for todo item

---
+++ src/components/ToDoItem/ToDoCheckBox/toDoCheckBox.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './toDoCheckBox.scss';
+
+type ToDoCheckboxProps = {
+  isChecked: boolean;
+  handler: (boxState: boolean) => void;
+};
+
+const ToDoCheckbox = ({ isChecked, handler }: ToDoCheckboxProps) => {
+  const [checkboxState, setCheckboxState] = useState(isChecked);
+
+  return (
+    <div className="todo-checkbox">
+      <label className="todo-checkbox-label">
+        <input
+          type="checkbox"
+          checked={checkboxState}
+          onChange={() => {
+            const state = checkboxState;
+            setCheckboxState(!state);
+            handler(!state);
+          }}
+        />
+        {checkboxState ? '🗹' : '☐'}
+      </label>
+    </div>
+  );
+};
+
+export default ToDoCheckbox;
