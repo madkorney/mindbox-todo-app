ed9d6bcff818a25f1e0bfe51c48052dcc7a9bebc
korney <madkorney@gmail.com>
2022-11-01 16:58:53 +0300

    refactor: update displayed list items number to be calculated based on window size

--- src/components/ToDoList/toDoList.tsx
+++ src/components/ToDoList/toDoList.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 
 import ToDoItem from 'components/ToDoItem/toDoItem';
 
-import { ToDoListItem, ItemsNumberToDisplay, UpdateHandler } from '../../data/data';
+import { ToDoListItem, UpdateHandler } from '../../data/data';
 
 type ToDoListProps = {
   todoList: ToDoListItem[];
   updateHandler: UpdateHandler;
   scrollIndexPosition: number;
+  itemsToDisplayMax: number;
 };
 
-const ToDoList = ({ todoList, updateHandler, scrollIndexPosition }: ToDoListProps) => {
+const ToDoList = ({
+  todoList,
+  updateHandler,
+  scrollIndexPosition,
+  itemsToDisplayMax,
+}: ToDoListProps) => {
   return (
     <div className="todo-list">
       <div className="list-items">
         {todoList.map((item, index) => {
-          return index >= scrollIndexPosition &&
-            index < scrollIndexPosition + ItemsNumberToDisplay ? (
+          return index >= scrollIndexPosition && index < scrollIndexPosition + itemsToDisplayMax ? (
             <ToDoItem listItem={item} key={item.id} updateHandler={updateHandler} />
           ) : null;
         })}
       </div>
-      <div className="list-controls"></div>
     </div>
   );
 };
