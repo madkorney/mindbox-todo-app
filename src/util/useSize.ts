a07b3150efe1c12c14c0c63a56dc4a867be93c79
korney <madkorney@gmail.com>
2022-11-01 16:56:59 +0300

    feat: implement hook to monitor window size

---
+++ src/util/useSize.ts
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+export type Size = {
+  width: number | undefined;
+  height: number | undefined;
+};
+
+// Usage
+// function App() {
+//   const size: Size = useWindowSize();
+//   return (
+//     <div>
+//       {size.width}px / {size.height}px
+//     </div>
+//   );
+// }
+
+// Hook
+export const useWindowSize = (): Size => {
+  const [windowSize, setWindowSize] = useState<Size>({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+};
