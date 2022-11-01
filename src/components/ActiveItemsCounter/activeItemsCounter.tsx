8b05264b8554e886299e54e1eddaa2e0f9116bc7
korney <madkorney@gmail.com>
2022-11-01 17:00:16 +0300

    refactor: update styles for status line component

--- src/components/ActiveItemsCounter/activeItemsCounter.tsx
+++ src/components/ActiveItemsCounter/activeItemsCounter.tsx
@@ -4,10 +4,20 @@ import './activeItemsCounter.scss';
 
 type CounterProps = {
   activeItemsQty: number;
+  totalItemsQty: number;
 };
 
-const ActiveItemsCounter = ({ activeItemsQty }: CounterProps) => {
-  return <div className="counter">{activeItemsQty}</div>;
+const ActiveItemsCounter = ({ activeItemsQty, totalItemsQty }: CounterProps) => {
+  return (
+    <div className="status-line">
+      <div className="active-items-num-label-long">active items:</div>
+      <div className="active-items-num-label-short">active/total:</div>
+      <div className="output">{activeItemsQty.toString().padStart(2, '0')}</div>
+      <div className="all-items-num-label-long">total items:</div>
+      <div className="all-items-num-label-short">/</div>
+      <div className="output">{totalItemsQty.toString().padStart(2, '0')}</div>
+    </div>
+  );
 };
 
 export default ActiveItemsCounter;
