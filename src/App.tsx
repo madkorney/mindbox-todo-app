994b22f7697234d8412c3a1c6888eafbec5856cf
korney <madkorney@gmail.com>
2022-11-02 12:51:45 +0300

    refactor: remove unused ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ToDoList from 'components/ToDoList/toDoList';
 import FilterTabs from 'components/FilterTabs/filterTabs';
